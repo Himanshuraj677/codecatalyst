8216e3545e77effb6f0ef2ea8c3dea0eb077a301
Himanshu Raj <himanshuraj6771@gmail.com>
2025-10-30 15:41:29 +0530

    handled mistypes during create course

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: NextRequest) {
       data: {
         name,
         description,
-        userId,
+        instructorId: userId,
         joinCode,
       },
     });
